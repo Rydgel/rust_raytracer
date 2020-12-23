823783f498260b882c227c5530e18e5d5cfe567a
Jérôme Mahuet <jerome.mahuet@gmail.com>
2020-12-23 20:30:29 +0100

    impl vec3

--- lib/structures/vec3.rs
+++ lib/structures/vec3.rs
@@ -1,4 +1,4 @@
-use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub};
+use std::ops::{Add, AddAssign, Div, DivAssign, Index, IndexMut, Mul, MulAssign, Neg, Sub};
 
 #[derive(Debug, Default)]
 pub struct Vec3([f32; 3]);
@@ -9,12 +9,166 @@ impl Vec3 {
     }
 
     pub fn x(&self) -> f32 {
-        self.0[0]
+        self[0]
     }
     pub fn y(&self) -> f32 {
-        self.0[1]
+        self[1]
     }
     pub fn z(&self) -> f32 {
-        self.0[2]
+        self[2]
+    }
+
+    pub fn is_empty(&self) -> bool {
+        false
+    }
+
+    pub fn len(&self) -> f32 {
+        self.squared_len().sqrt()
+    }
+
+    pub fn squared_len(&self) -> f32 {
+        self.x() * self.x() + self.y() * self.y() + self.z() * self.z()
+    }
+}
+
+impl Index<usize> for Vec3 {
+    type Output = f32;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.0[index]
+    }
+}
+
+impl IndexMut<usize> for Vec3 {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.0[index]
+    }
+}
+
+impl Add for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, othr: Vec3) -> Self::Output {
+        Self([
+            self.x() + othr.x(),
+            self.y() + othr.y(),
+            self.z() + othr.z(),
+        ])
+    }
+}
+
+impl AddAssign for Vec3 {
+    fn add_assign(&mut self, othr: Vec3) {
+        self[0] += othr[0];
+        self[1] += othr[1];
+        self[2] += othr[2];
+    }
+}
+
+impl Sub for Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, othr: Vec3) -> Self::Output {
+        Self([
+            self.x() - othr.x(),
+            self.y() - othr.y(),
+            self.z() - othr.z(),
+        ])
+    }
+}
+
+impl Mul for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, othr: Vec3) -> Self::Output {
+        Self([
+            self.x() * othr.x(),
+            self.y() * othr.y(),
+            self.z() * othr.z(),
+        ])
+    }
+}
+
+impl Mul<f32> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, othr: f32) -> Self::Output {
+        Self([self.x() * othr, self.y() * othr, self.z() * othr])
+    }
+}
+
+impl Mul<Vec3> for f32 {
+    type Output = Vec3;
+
+    fn mul(self, othr: Vec3) -> Self::Output {
+        othr * self
+    }
+}
+
+impl MulAssign for Vec3 {
+    fn mul_assign(&mut self, othr: Vec3) {
+        self[0] *= othr[0];
+        self[1] *= othr[1];
+        self[2] *= othr[2];
+    }
+}
+
+impl MulAssign<f32> for Vec3 {
+    fn mul_assign(&mut self, othr: f32) {
+        self[0] *= othr;
+        self[1] *= othr;
+        self[2] *= othr;
+    }
+}
+
+impl Div for Vec3 {
+    type Output = Vec3;
+
+    fn div(self, othr: Vec3) -> Self::Output {
+        Self([
+            self.x() / othr.x(),
+            self.y() / othr.y(),
+            self.z() / othr.z(),
+        ])
+    }
+}
+
+impl Div<f32> for Vec3 {
+    type Output = Vec3;
+
+    fn div(self, othr: f32) -> Self::Output {
+        Self([self.x() / othr, self.y() / othr, self.z() / othr])
+    }
+}
+
+impl Div<Vec3> for f32 {
+    type Output = Vec3;
+
+    fn div(self, othr: Vec3) -> Self::Output {
+        othr / self
+    }
+}
+
+impl DivAssign for Vec3 {
+    fn div_assign(&mut self, othr: Vec3) {
+        self[0] /= othr[0];
+        self[1] /= othr[1];
+        self[2] /= othr[2];
+    }
+}
+
+impl DivAssign<f32> for Vec3 {
+    fn div_assign(&mut self, othr: f32) {
+        self[0] /= othr;
+        self[1] /= othr;
+        self[2] /= othr;
+    }
+}
+
+impl Neg for Vec3 {
+    type Output = Vec3;
+
+    fn neg(self) -> Self::Output {
+        Self([-self.x(), -self.y(), -self.z()])
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use num_iter::range_step;
-use raytracer_lib::output::png::write_pixels_to_file;
+use raytracer_lib::{output::png::write_pixels_to_file, structures::vec3::Vec3};
 
 fn main() {
     let nx = 200;
@@ -8,12 +8,13 @@ fn main() {
 
     for j in range_step(ny as i32, 0, -1) {
         for i in range_step(0, nx as i32, 1) {
-            let r = i as f64 / nx as f64;
-            let g = j as f64 / ny as f64;
-            let b = 0.2f64;
-            let ir = (255.99 * r).floor() as u8;
-            let ig = (255.99 * g).floor() as u8;
-            let ib = (255.99 * b).floor() as u8;
+            let r = i as f32 / nx as f32;
+            let g = j as f32 / ny as f32;
+            let b = 0.0f32;
+            let col = Vec3::new(r, g, b);
+            let ir = (255.99 * col[0]).floor() as u8;
+            let ig = (255.99 * col[1]).floor() as u8;
+            let ib = (255.99 * col[2]).floor() as u8;
 
             pixels.push(ir);
             pixels.push(ig);
